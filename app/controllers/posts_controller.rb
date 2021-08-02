16abde24a2172964ea72bb4578faee704d7c64a3
EliezerAmbion <eliezerambion@gmail.com>
2021-08-02 19:06:47 +0800

    add controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,37 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all.order("created_at DESC")
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to root_path
+
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to root_path, notice: "Successfully deleted"
+  end
+
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content, :rating, :date)
+  end
+end
