c433e027ed18b1228fffb68cbc10bef2b3e1b1d9
EliezerAmbion <eliezerambion@gmail.com>
2021-08-20 11:15:41 +0800

    add show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,19 +12,37 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
 
     if @post.save
-      redirect_to root_path
+      redirect_to root_path, notice: "Successfully created a post"
 
     else
-      render "new"
+      render "new", alert: @post.errors.full_messages.first
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to root_path, notice: "Successfully updated post"
+
+    else
+      render "edit", alert: @post.errors.full_messages.first
     end
   end
 
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
-    redirect_to root_path, notice: "Successfully deleted!"
+    redirect_to root_path, notice: "Successfully deleted post"
   end
-
 
   private
 
